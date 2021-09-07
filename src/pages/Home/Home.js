0c48e41d2f43ba231a90b01668493325a11f9ea5
Jonathan Cedeño <jony.cede@gmail.com>
2021-09-07 13:53:46 +0200

    First implementation of Home Page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import "./Home-style.css";
 
 function Home() {
+  const [searchValue, setSearchValue] = useState("");
+  const history = useHistory();
+
+  function handleChange(e) {
+    setSearchValue(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    history.push("/search");
+  }
+
   return (
-    <>
-      <div>Home</div>
-    </>
+    <main>
+      <div id="main-left">
+        <p id="page-description-title">Simple and direct.</p>
+        <p id="page-description-body">
+          Just type the name of the city you are looking for and we will do the
+          job for you. What are you waiting for to find the home you deserve?
+        </p>
+      </div>
+      <div id="main-right">
+        <form onSubmit={handleSubmit}>
+          <img src="./icons/icon-search.png" alt="icon" />
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchValue}
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+    </main>
   );
 }
 
